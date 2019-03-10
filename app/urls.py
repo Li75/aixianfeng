fc2b1a20c14d2245a8236c6c9e5b739d1d979967
Your Name <you@example.com>
2019-03-10 16:52:45 +0800

    爱鲜蜂首页,闪购超市,我的,注册完成

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from app import views
+
+urlpatterns = [
+    url(r'^$',views.home,name='home'),
+    url(r'^market/$',views.market,name='market'),
+    url(r'^market/(?P<childid>\d+)/(?P<sortid>\d+)/$', views.market, name='market'),
+    url(r'^cart/$',views.cart,name='cart'),
+    url(r'^mine/$',views.mine,name='mine'),
+    # url(r'^base/$',views.base,name='base'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^login/$',views.login,name='login'),
+    url(r'^logout/$',views.logout,name='logout')
+
+]
---
+++ app/views.py
@@ -0,0 +1,150 @@
+import hashlib
+import random
+import time
+
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.core.cache import cache
+
+from app.models import Wheel, Nav, Mustbuy, Shop, Foodtype, Goods, User
+
+
+def home(request):
+    wheels = Wheel.objects.all()
+
+    navs = Nav.objects.all()
+
+    mustbuys = Mustbuy.objects.all()
+
+    shops = Shop.objects.all()
+    shophead = shops[0]
+    shoptabs = shops[1:3]
+    shopclass_list = shops[3:7]
+    shopcommends = shops[7:11]
+
+    response_dir = {
+        'wheels': wheels,
+        'navs':navs,
+        'mustbuys':mustbuys,
+        'shophead':shophead,
+        'shoptabs':shoptabs,
+        'shopclass_list':shopclass_list,
+        'shopcommends':shopcommends,
+    }
+    return render(request, 'home/home.html', context=response_dir)
+
+
+def market(request, childid='0', sortid='0'):
+
+    foodtypes = Foodtype.objects.all()
+    # 商品信息
+    # goods_list = Goods.objects.all()[0:5]
+    # 默认打开页面  热销榜
+    # 点击左侧分类，即显示对应分类 商品信息  【传参数categoryid】
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+
+    # 客户端 需要记录 点击的分类下标 【cookies， 会自动携带】
+    index = int(request.COOKIES.get('index', '0'))
+    # 根据index 获取 对应的 分类ID
+    categoryid = foodtypes[index].typeid
+    # 根据 分类ID 获取对应分类信息
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+
+    # 子类
+    if childid == '0':
+        goods_list = Goods.objects.filter(categoryid=categoryid)
+    else:
+        goods_list = Goods.objects.filter(categoryid=categoryid).filter(childcid=childid)
+
+    # 排序
+    # 0默认综合排序   1销量排序     2价格最低   3价格最高
+    if sortid == '1':
+        goods_list = goods_list.order_by('-productnum')
+    elif sortid == '2':
+        goods_list = goods_list.order_by('price')
+    elif sortid == '3':
+        goods_list = goods_list.order_by('-price')
+
+    # 获取子类信息
+    childtypenames = foodtypes[index].childtypenames
+    # 存储子类信息 列表
+    childtype_list = []
+    # 将对应的子类拆分出来
+    for item in childtypenames.split('#'):
+        # item   全部分类:0
+        # item   子类名称: 子类ID
+        item_arr = item.split(':')
+        temp_dir = {
+            'name': item_arr[0],
+            'id': item_arr[1]
+        }
+
+        childtype_list.append(temp_dir)
+
+    response_dir = {
+        'foodtypes': foodtypes,
+        'goods_list': goods_list,
+        'childtype_list': childtype_list,
+        'childid': childid
+    }
+    print(response_dir)
+    return render(request,'market/market.html', context=response_dir)
+
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def mine(request):
+    token = request.session.get('token')
+    user = None
+    if token:
+        user = User.objects.get(pk=token)
+    return render(request, 'mine/mine.html',context={'user':user})
+
+
+# def base(request):
+#     return render(request,'base/base.html')
+
+
+def generate_password(param):
+    md5 = hashlib.md5()
+    md5.update(param.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def generate_token():
+    temp = str(time.time()) + str(random.random())
+    md5 = hashlib.md5()
+    md5.update(temp.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request,'mine/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = generate_password(request.POST.get('password'))
+        phone = request.POST.get('phone')
+
+        user = User()
+        user.username = username
+        user.password = password
+        user.phone = phone
+        user.save()
+
+        token = generate_token()
+        request.session['token'] = token
+        return redirect('app:mine')
+
+
+def login(request):
+    return render(request,'mine/login.html')
+
+
+def logout(request,):
+    request.session.flush()
+    return redirect('app:mine')
